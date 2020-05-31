3cc517d595c8f1942848334df378eb6c5da3306b
Santosh <santoshk87@gmail.com>
2020-05-31 09:55:38 +0530

    add space

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React,{ Suspense, lazy } from 'react';
 //import logo from './logo.svg';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import main from './components/css/main.css'
-import Home from './components/pages/Home';
+const Home = lazy(() => import('./components/pages/Home'));
+const RenderLoader= ()=><h1>Loading...</h1>;
 //import './App.css';
 
 function App() {
   return (
+    <Suspense fallback={RenderLoader()}>
     <Home />
+    </Suspense>
     );
-}
+}  
 
 export default App;

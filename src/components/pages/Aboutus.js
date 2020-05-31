d755eae74f6bf75a903e5b3a05e21f791c179af6
Santosh <santoshk87@gmail.com>
2020-05-31 09:58:01 +0530

    add about page

---
+++ src/components/pages/Aboutus.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+class Aboutus extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About us</h1>
+            </div>
+        )
+    }
+}
+
+export default Aboutus

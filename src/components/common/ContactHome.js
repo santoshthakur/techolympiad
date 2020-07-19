13b38471780e6e1bcebda9d69c93c92915d23f53
santosh <santoshk87@gmail.com>
2020-07-19 16:02:15 +0530

    testing

--- src/components/common/ContactHome.js
+++ src/components/common/ContactHome.js
@@ -6,7 +6,7 @@ function ContactHome() {
         <div className="col-lg-12 pb-5 contact-home">
             <div className="container">
                 <div className="col-lg-6 offset-lg-6">
-                    <h2 className="text-left mt-5 pb-3 text-light">Contact 2222us1111</h2>
+                    <h2 className="text-left mt-5 pb-3 text-light">Contact 22    22gitus1111</h2>
                     <p className="text-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut maecenas accumsan lacus vel facilisis.</p>
                     <ContctForm />
                 </div>
